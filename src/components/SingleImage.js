c7bf917131f355a7590db664a22eb3f64d903632
CaveMan <caveman@Computer.local>
2020-01-07 11:45:42 +0000

    Fixed the out of range issue with the prev and next buttons in single view

--- src/components/SingleImage.js
+++ src/components/SingleImage.js
@@ -1,30 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import path from 'path';
 
 export const SingleImage = (props) => {
-//	const {params} = this.props.match.params;	// Make an object out of the URL perameters
 	const urlParams = new URLSearchParams(window.location.search);	// Make an object out of the URL perameters
-	let next = {
-		file: props.getJpegs[parseInt(urlParams.get('index')) + 1],
-		index: parseInt(urlParams.get('index')) + 1
+
+	let currentFile = {
+		file: props.getJpegs[parseInt(urlParams.get('index'))],
+		index: parseInt(urlParams.get('index'))
 	}
 
-	const prev = {
-		file: props.getJpegs[parseInt(urlParams.get('index')) - 1],
-		index: parseInt(urlParams.get('index')) - 1
+	let next = {...currentFile}
+	let prev = {...currentFile}
+
+	if (parseInt(urlParams.get('index')) < parseInt(props.getJpegs.length) - 1) {
+		next.file = props.getJpegs[parseInt(urlParams.get('index')) + 1];
+		next.index ++;
+	}
+
+	if (parseInt(urlParams.get('index')) > 0) {
+		prev.file = props.getJpegs[parseInt(urlParams.get('index')) - 1];
+		prev.index --;
 	}
 
 	return(
 		<div className="single-container">
 			<div>
 				<img
-					alt={"File Not Found " + urlParams.get('fileName')}
+					alt={"File Not Found " + JSON.stringify(urlParams.get('fileName'))}
 					className="single-image"
-					src={
-						process.env.PUBLIC_URL
-							+ "../images/resize1024/"
-							+ urlParams.get('fileName')
-					}
+					src={path.join(
+						process.env.PUBLIC_URL,
+						'../images/resize1024/',
+						urlParams.get('fileName')
+					)}
 				/>
 
 				<Link
