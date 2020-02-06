2312a9254f2d86dcf4cd1def8120d57f38c4e1c0
Dan <dan@waxworlds.org>
2020-02-06 18:51:04 +0000

    The image transitions finally work!!! Only when you're viewing the gallery though!!! (i'll fix that next)
     I think the add remove button in single view is broken so I need to
     fix that.
    
    I also don't think the urls work if you share them so I'd like to get
    that working too.
    
    need to tweak the CSS a bit.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,14 +4,18 @@ import { useTransition, animated } from 'react-spring';
 
 export const Slider = (index, jpegs, props) => {
 
-	const transitions = useTransition(jpegs[index], jpegs => jpegs.indexOf, {
+	const item = jpegs[index];
+
+	const transitions = useTransition(jpegs[index], item => item.id, {
 		from: {opacity: 0, transform: 'scale(1.2)'},
 		enter: {opacity: 1, transform: 'scale(1)'},
-		leave: {opacity: 0, transform: 'scale(0.8)'}
+		leave: {opacity: 0, transform: 'scale(0.8)'},
 	})
 
-	console.log(index + ' ' + jpegs)
+	console.log(index + ' ' + jpegs[index].file)
 	console.log(window.location)
+
+
 
 	return transitions.map(({item, props, key}) => {
 		return (
@@ -19,7 +23,7 @@ export const Slider = (index, jpegs, props) => {
 				className={'single-image'}
 				key={key}
 				style={{
-					backgroundImage: `url(/images/resize1024/${encodeURIComponent(jpegs[index])})`,
+					backgroundImage: `url(/images/resize1024/${encodeURIComponent(item.file)})`,
 					...props
 				}}
 			/>
