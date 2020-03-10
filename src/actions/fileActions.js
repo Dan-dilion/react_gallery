d7282f17edf347aefb2a1e03a6d3380247a6af45
Dan <dan@waxworlds.org>
2020-03-10 19:01:17 +0000

    Made some minor changes to the refreshJpegs debug messages.
    Changed the add and remove icons to SVG vector icons.
    
    I have adjusted the colour of the minus icon by editing the icon file
    and adding style fill and colour attributes.

--- src/actions/fileActions.js
+++ src/actions/fileActions.js
@@ -2,20 +2,52 @@ import { getJpegs } from '../utils/serverRequest.js';
 import store from '../store.js';
 
 export function addJpegs(jpegs) {
-	return {
+  return {
 		type: "ADD_JPEGS",
 		payload: jpegs
-	};
+	}
 };
 
-export function refreshJpegs() {return dispatch => {
-  if (store.getState().fileReducer.jpegs.length == 0) {
-    try {
-      getJpegs()
-      .then(newJpegs => dispatch(addJpegs(newJpegs)))
-    } catch (error) {
-      console.log('fileActions.js - refreshJpegs: ERROR!!!', error)
-    }
-    console.log('File Action - Jpegs Refreshed.')
-  } else console.log('FileActions - Refresh Jpegs Unnecessary!')
+export function refreshJpegs() { return async dispatch => {
+  console.log('FileActions, refresh called - jpegs.length: ', store.getState().fileReducer.jpegs.length)
+  if (store.getState().fileReducer.jpegs.length === 0) {
+
+    await getJpegs()
+      .then( async response => await dispatch( await addJpegs(response) ) )
+      .then( console.log('File Action - Jpegs Refreshed.', store.getState().fileReducer.jpegs.length) )
+      .catch( error => console.log('fileActions.js - refreshJpegs: ERROR!!!', error ) )
+
+  } else console.log('FileActions - Refresh Jpegs Unnecessary! ', store.getState().fileReducer.jpegs.length)
 }}
+
+// export function refreshJpegs() {
+//   console.log('FileActions, refresh called - jpegs.length: ', store.getState().fileReducer.jpegs.length)
+//   return async dispatch => {
+//
+//     function onSuccess(success) {
+//       dispatch({
+//     		type: "ADD_JPEGS",
+//     		payload: success
+//     	});
+//       console.log('File Action - Jpegs Refreshed.', store.getState().fileReducer.jpegs.length)
+//       return success;
+//     }
+//
+//     function onError(error) {
+//       console.log('fileActions.js - refreshJpegs: ERROR!!!', error);
+//       return error;
+//     }
+//
+//     try {
+//       //if (store.getState().fileReducer.jpegs.length === 0){
+//         const success = await getJpegs();
+//         return onSuccess(success);
+//       //} else {
+//     //    throw('Refresh Jpegs Action: ERROR! No need to refresh')
+//     //  }
+//     } catch (error) {
+//       return onError(error);
+//     }
+//   }
+//
+// }
