6a681dce4ee010846357edd7dfed3b7769e99369
Dan <dan@waxworlds.org>
2020-03-22 22:13:39 +0000

    Added different grid size for smaller devices
    I have added a css media query that will adjust the grid size depending
    on the viewport width.
    
    I have also rewritten some of the image pattern sequencer's logic so
    that it can accommodate smaller grid sizes.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,10 @@
 export const resequenceJpegs = (jpegs) => {
 
-  let debug = 0;                    // Turn on/off debug info in the console.
+  let debug = 1;                    // Turn on/off debug info in the console.
   let newSequence = [];
+  let gridWidth = 12;
+  if (window.matchMedia("(max-width: 800px)").matches) gridWidth = 6;
+  console.log('Grid Width = ', gridWidth)
 
   const logit = (...message) => {
     if (debug) {
@@ -33,28 +36,39 @@ export const resequenceJpegs = (jpegs) => {
   logit('Number of Portraits = ', portraits.length)
   logit('Number of Landscapes = ', landscapes.length)
 
-  const getNumOfRows = () => {                                                            // Function to predict the number of rows after image distribution
-    let predictedRows = parseInt(((landscapes.length * 2) + portraits.length) / 12)       // landscapes take twice as much space as portraits
-    if ((((landscapes.length * 2) + portraits.length) % 12) > 0) predictedRows++;         // if there is a remainder add a final row
-    return predictedRows;                                                                 // return prediction
-  }
-  const predictedNumOfRows = getNumOfRows()
-  logit('Predicted Num Of Rows: ', predictedNumOfRows)
+  const predictedNumOfRows =  Math.ceil(((landscapes.length * 2) + portraits.length) / gridWidth)       // landscapes take twice as much space as portraits,
+  logit('Predicted Num Of Rows: ', predictedNumOfRows)                                                  // round result up if not an integer
 
-  const sequencer = (numOfPortraits = 0, offset = 0) => {                                    // This is the sequencer!
-    let pattern = '111111';                                                                  // if no numberOfPortraits is given the default will be none
-    switch(numOfPortraits) {                                                                 //
-      case  2: if (offset) pattern = '0111110'; else pattern = '1101011'; break;             // alternate patterns for offset and not (even and odd)
-      case  4: if (offset) pattern = '01011010'; else pattern = '10100101'; break;           //
-      case  6: if (offset) pattern = '010010010'; else pattern = '100010001'; break;         //
-      case  8: if (offset) pattern = '0010000100'; else pattern = '1000000001'; break;       //
-      case 10: pattern = '00000100000'; break;                                               // only one symmetrical pattern can be made
-      case 12: pattern = '000000000000'; break;                                              // only one symmetrical pattern can be made
-      default: break;
+/*                     **
+**  Pattern sequencer  **
+**                     */
+  const sequencer = (numOfPortraits = 0, offset = 0) => {                                      // This is the sequencer!
+    let pattern = [];
+    if (gridWidth == 12) {
+      pattern = '111111';                                                                      // if no numberOfPortraits is given the default will be none
+      switch(numOfPortraits) {                                                                 //
+        case  2: if (offset) pattern = '0111110'; else pattern = '1101011'; break;             // alternate patterns for offset and not (even and odd)
+        case  4: if (offset) pattern = '01011010'; else pattern = '10100101'; break;           //
+        case  6: if (offset) pattern = '010010010'; else pattern = '100010001'; break;         //
+        case  8: if (offset) pattern = '0010000100'; else pattern = '1000000001'; break;       //
+        case 10: pattern = '00000100000'; break;                                               // only one symmetrical pattern can be made
+        case 12: pattern = '000000000000'; break;                                              // only one symmetrical pattern can be made
+        default: break;
+      }
+    }
+    if (gridWidth == 6) {
+      pattern = '111'
+      if (numOfPortraits > 0) {
+        if (offset) pattern = '0110';
+        else pattern = '1001';
+      }
     }
 
-    logit('Pattern: ', pattern);
+    logit('Pattern: ' + pattern + ' gridWidth: ' + gridWidth + ' numOfPortraits: ' + numOfPortraits);
 
+/*                    **
+**  Image Distributer **
+**                    */
     [...pattern].forEach(item => {                                                           // convert the pattern in to an array and begin itterating through it's characters
       if (parseInt(item) && landscapes.length > 0) newSequence.push(landscapes.shift())      // if item is a 1 and there are landscapes left push landscape
       else if (portraits.length > 0) newSequence.push(portraits.shift())                     // otherwise if there are any portraits left push portrait
@@ -62,42 +76,52 @@ export const resequenceJpegs = (jpegs) => {
     });
   }
 
-  let offsetToggle = 0;
-  let leftOverRows = 0;
+
+  let offsetToggle = 1;
+//  let leftOverRows = 0;
+  let leftoverPorts = 0;
   let oddPorts = 0;
-  let evenPorts = 2;
-  let portsPerRow = portraits.length / predictedNumOfRows
+  let evenPorts = 0;
+  let portsPerRow = portraits.length / predictedNumOfRows;
+  //let rowsPerPortraitPair = (portraits.length / 2)
 
-  if (portsPerRow <= 1) evenPorts = 2;                   // if there's less or the same number of portraits than the number of rows
-  else if (portsPerRow <= 2) evenPorts = 2;              // if there's less than or the same number of portraits for two per row
-  else if (portsPerRow <= 4) evenPorts = 4;              // if there's less than or the same number of portraits for four per row
-  else if (portsPerRow <= 6) evenPorts = 6;              // if there's less than or the same number of portraits for six per row
-  else if (portsPerRow <= 8) evenPorts = 8;              // if there's less than or the same number of portraits for eight per row
-  else if (portsPerRow <= 10) evenPorts = 10;            // if there's less than or the same number of portraits for ten per row
-  else evenPorts = 12;                                                               // if there's less than or the same number of portraits for twelve per row
-  logit('Case Selector: ', portsPerRow )
+/*                                **
+**  Logical Portraits Distributer **
+**                                */
 
-  if (portraits.length > predictedNumOfRows) {                                       // if there is enough portraits to offset every other row
-    leftOverRows = Math.ceil(                                                        // rounds a real number up to the nearest integer even if less than 0.5 over
-      ((portraits.length - ((predictedNumOfRows / 2) * evenPorts)) / evenPorts)      // number of rows that can be filled with the left over portraits (after offset rows)
-    );
-  }
-  logit('LeftOver: ', leftOverRows)
+// this next loop decides how many time you can distribute 2 portraits on every other row
+// Each time through the loop it either adds two portraits to the even rows or two to the odd rows
+// If there is not enough portraits left for a whole pass they are added to leftoverPorts
 
-  for ( let i = predictedNumOfRows; i > 0; i -- ) {                         // Iterate round this loop once for each predicted row
-    if (leftOverRows > 0 && !offsetToggle) {                                      // if there is enough portraits for the even rows also distribute in odd rows
-      oddPorts = evenPorts;                                                 // distribute the same number of portraits in the odd rows as the even
-      leftOverRows --;                                                      // decreace the number of leftOverRows
+  if (portraits.lenghth < predictedNumOfRows) evenPorts = 2;     // if there not enough portraits for the offset rows
+  else {                                                         // use the portraits for the offset rows only
+    let i = portraits.length;
+    do {
+      i -= predictedNumOfRows;                                   // each time round the loop deduct 2 portraits for half the number of rows
+      if (offsetToggle) evenPorts += 2;                          // every other time round the loop either 2 portraits will be added on to
+      else oddPorts += 2;                                        // evenPorts or 2 will be added on to oddPorts.
+      offsetToggle = !offsetToggle;                              // this switches offsetToggle
     }
-    else oddPorts = 0;                                                      // if there is only enough portraits left for the even rows do not distribute in odd rows
-    if (!offsetToggle) sequencer(oddPorts, offsetToggle)                                // if on odd numbered row call the sequencer and pass in num of odd portraits
-    else sequencer(evenPorts, offsetToggle)                                       // else call sequencer and pass in num of even portraits
-    offsetToggle = !offsetToggle;                                                       // Toggle offset
-
-    logit('itteration: ', i)
-    logit('left over rows: ', leftOverRows)
-    logit('OddPorts: ' + oddPorts + ' even Ports: ' + evenPorts)
+    while (i > predictedNumOfRows);                              // if there is not enough portraits left to accomodate another pass of the loop
+    leftoverPorts = i;                                           // break out of the loop and add the leftover portraits to leftoverPorts
   }
+
+// This next loop calls the sequencer for each row and distributes leftoverPorts amonst the odd numbered rows
+
+  offsetToggle = 0;                                      // Reset the toggle
+  for (let i = predictedNumOfRows; i > 0; i--) {         // loop once for every row
+    if (!offsetToggle) {                                 // if its an odd numbered row
+      if (leftoverPorts > 0) {                           // call the sequencer and pass in the number of oddPorts
+        sequencer(oddPorts + 2, offsetToggle)            // if there are leftoverPorts left add 2 to the number
+        leftoverPorts -= 2;                              // of oddPorts and deduct 2 from the number of leftoverPorts
+      } else sequencer(oddPorts, offsetToggle);          // if there's no leftoverPorts just pass in the oddPorts
+    }
+    else sequencer(evenPorts, offsetToggle);             // otherwise its an even row, call sequencer and pass in evenPorts
+
+    offsetToggle = !offsetToggle;                        // switch the toggle
+  }
+
+  logit('Case Selector: ' + portsPerRow + ' evenPorts: ' + evenPorts)
 
   console.log('Array resequenced!!! num of items = ', newSequence.length)
   return newSequence;
